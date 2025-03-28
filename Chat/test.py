6634a5ad36b11f013914deefc4aa3b37a13be469
medsalahtammar <mohamedsalah.tammar@tanit.ai>
2025-03-28 12:42:41 +0000

    Literal AI

--- Chat/test.py
+++
@@ -1,3 +0,0 @@
-from whatsapp_utils import find_inactive_numeric_users,notify_user
-
-print(notify_user("21699363307"))
--- Chat/weightsandbiases.py
+++
@@ -1,5 +0,0 @@
-from Chat_function_literal import chat_with_gpt4_no_streaming_wandb
-import weave
-
-weave.init('tanit')
-print(chat_with_gpt4_no_streaming_wandb("and what is male infertility?")) 
